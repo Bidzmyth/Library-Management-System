1763bf3f1cc8dd8957901441807662ee23f55355
Bidzmyth <biditabhattacharya143@gmail.com>
2026-01-16 10:58:34 +0530

    Author entity created

--- LibraryManagement/src/main/java/com/ey/domain/entity/Author.java
+++ LibraryManagement/src/main/java/com/ey/domain/entity/Author.java
@@ -1,5 +1,28 @@
 package com.ey.domain.entity;
 
-public class Author {
 
+import jakarta.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "authors")
+public class Author extends BaseEntity {
+ @Column(nullable = false, length = 120)
+ private String fullName;
+
+ @Column(length = 500)
+ private String bio;
+
+ public String getFullName() { return fullName; }
+ public String getBio() { return bio; }
+ public void setFullName(String fullName) { this.fullName = fullName; }
+ public void setBio(String bio) { this.bio = bio; }
+
+ @Override public boolean equals(Object o) {
+     if (this == o) return true;
+     if (!(o instanceof Author a)) return false;
+     return Objects.equals(getId(), a.getId());
+ }
+ @Override public int hashCode() { return Objects.hash(getId()); }
 }
+
